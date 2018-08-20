48350a4d873c245511b3aa42d1ab9a3d2deafb1a
JeroChan <Zero@Zander-PC.unitek.com>
2018-08-20 14:38:58 +0800

    Only authorized user can edit user profile

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -66,7 +66,7 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        return \App\Models\User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
